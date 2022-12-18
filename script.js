560a551336a2e21b767f012a03a9c76a6c99928e
sadam <sadamccr@gmail.com>
2022-12-19 03:17:17 +0500

    adding removing elements

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // const body = document.body
 
 // const div = document.createElement("div")
-// div.innerText = ('hello')
+// div.innerText = 'hello'
+
+// div.innerHTML = '<strong>my world</strong>'
 
 // body.append(div)
+
+// //------- removing | adding ELEMENTS to document--------------
+
+// const body = document.body
+
+// const div = document.querySelector('div')
+
+// const spanHi = document.querySelector('#hi')
+
+// const spanBye = document.querySelector('#bye')
+
+// spanBye.remove()
+
+// body.append(spanBye)
+
+
